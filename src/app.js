ec31aaef085be14a0264b4c3aebba7c629e5154c
Jorge Sandoval <sandovl06roque@gmal.com>
2024-01-06 14:43:48 -0800

    Compactando rutas

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ import unitRoutesEs from './routes/unitRoutesEs.js'
 import unitRoutesJp from './routes/unitRoutesJp.js'
 import dataRoutes from './routes/dataRoutes.js'
 import formRoutes from './routes/formRoutes.js'
-import formPostRoutes from './routes/formPostRoutes.js'
-import formUpdateRoutes from './routes/formUpdateRoutes.js'
-import formDeleteRoutes from  './routes/formDeleteRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT ?? 4121
@@ -32,6 +29,3 @@ app.use('/es/unit', unitRoutesEs)
 app.use('/jp/unit', unitRoutesJp)
 app.use('/data', dataRoutes)
 app.use('/form', formRoutes)
-app.use('/form/post', formPostRoutes)
-app.use('/form/update', formUpdateRoutes)
-app.use('/form/delete', formDeleteRoutes)
--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -22,4 +22,64 @@ router.get('/types', (req, res)=>{
     res.send('<h1>Formulario TYPEs</h1>')
 })
 
+router.put('update/tags', (req, res)=>{
+    res.send('<h1>Formulario para modificar TAGs</h1>')
+})
+
+router.put('update/chapters', (req, res)=>{
+    res.send('<h1>Formulario para modificar CHAPTERs</h1>')
+})
+
+router.put('update/colors', (req, res)=>{
+    res.send('<h1>Formulario para modificar COLORs</h1>')
+})
+
+router.put('update/raritys', (req, res)=>{
+    res.send('<h1>Formulario para modificar RARITYs</h1>')
+})
+
+router.put('update/types', (req, res)=>{
+    res.send('<h1>Formulario para modificar TYPEs</h1>')
+})
+
+router.post('post/tags', (req, res)=>{
+    res.send('<h1>Formulario para subir TAGs</h1>')
+})
+
+router.post('post/chapters', (req, res)=>{
+    res.send('<h1>Formulario para subir CHAPTERs</h1>')
+})
+
+router.post('post/colors', (req, res)=>{
+    res.send('<h1>Formulario para subir COLORs</h1>')
+})
+
+router.post('post/raritys', (req, res)=>{
+    res.send('<h1>Formulario para subir RARITYs</h1>')
+})
+
+router.post('post/types', (req, res)=>{
+    res.send('<h1>Formulario para subir TYPEs</h1>')
+})
+
+router.delete('delete/tags', (req, res)=>{
+    res.send('<h1>Formulario para eliminar TAGs</h1>')
+})
+
+router.delete('delete/chapters', (req, res)=>{
+    res.send('<h1>Formulario para eliminar CHAPTERs</h1>')
+})
+
+router.delete('delete/colors', (req, res)=>{
+    res.send('<h1>Formulario para eliminar COLORs</h1>')
+})
+
+router.delete('delete/raritys', (req, res)=>{
+    res.send('<h1>Formulario para eliminar RARITYs</h1>')
+})
+
+router.delete('delete/types', (req, res)=>{
+    res.send('<h1>Formulario para eliminar TYPEs</h1>')
+})
+
 export default router
