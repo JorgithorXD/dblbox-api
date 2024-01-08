f096cdfbe84949f818b1524a1d7daf2ac4bddd61
Jorge Sandoval <sandovl06roque@gmal.com>
2024-01-08 15:57:19 -0800

    Separando el modo de obtencion de la db, ya le entiendo mas

--- src/controllers/getData.js
+++ src/controllers/getData.js
@@ -1,1 +1,39 @@
+import { supabase } from "../database/supabase.js"
 
+const getById = async (table, column_id, id) => {
+    try {
+        const { data, error } = await supabase
+            .from(table)
+            .select('*')
+            .eq(column_id, id)
+
+        if (error) {
+            throw error
+        }
+        return data
+    } catch (error) {
+        console.error('Error al obtener datos de Supabase:', error)
+        throw error
+    }
+};
+
+const getAll = async (table) => {
+    try {
+        const { data, error } = await supabase
+            .from(table)
+            .select('*')
+
+        if (error) {
+            throw error
+        }
+        return data
+    } catch (error) {
+        console.error('Error al obtener datos de Supabase:', error)
+        throw error
+    }
+}
+
+export const getData = {
+    getById,
+    getAll
+}
--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,37 +1,44 @@
-import express  from "express"
+import express from "express"
 import { supabase } from "../database/supabase.js"
+import { getData } from "../controllers/getData.js"
 
 const router = express.Router()
 
-router.get('/tags', async (req, res)=>{
-    try {
-        const { data, error } = await supabase.from('data_tags').select('*');
-    
-        if (error) {
-          throw error;
-        }
-    
-        res.status(200).json(data);
-      } catch (error) {
-        console.error('Error al obtener datos de Supabase:', error);
-        res.status(500).json({ error: 'Error al obtener datos de Supabase' });
-      }
+router.get('/tags', async (req, res) => {
+  try {
+    const data = await getData.getAll('data_tags')
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
 })
 
-router.get('/chapters', (req, res)=>{
-    res.send('<h1>TODOS LOS CHAPTERs</h1>')
+router.get('/tags/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await getData.getById('data_tags', 'tag_id', id)
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
 })
 
-router.get('/colors', (req, res)=>{
-    res.send('<h1>TODOS LOS COLORs</h1>')
+router.get('/chapters', (req, res) => {
+  res.send('<h1>TODOS LOS CHAPTERs</h1>')
 })
 
-router.get('/raritys', (req, res)=>{
-    res.send('<h1>TODOS LOS RARITYs</h1>')
+router.get('/colors', (req, res) => {
+  res.send('<h1>TODOS LOS COLORs</h1>')
 })
 
-router.get('/types', (req, res)=>{
-    res.send('<h1>TODOS LOS TYPESs</h1>')
+router.get('/raritys', (req, res) => {
+  res.send('<h1>TODOS LOS RARITYs</h1>')
+})
+
+router.get('/types', (req, res) => {
+  res.send('<h1>TODOS LOS TYPESs</h1>')
 })
 
 export default router
