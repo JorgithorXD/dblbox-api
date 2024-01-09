d72c6f2de19e88f9f0e0515a3f20821fc5147f5e
Jorge Sandoval <sandovl06roque@gmal.com>
2024-01-08 17:59:50 -0800

    Completando GET de dataRoutes

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -25,20 +25,89 @@ router.get('/tags/:id', async (req, res) => {
   }
 })
 
-router.get('/chapters', (req, res) => {
-  res.send('<h1>TODOS LOS CHAPTERs</h1>')
+router.get('/chapters', async (req, res) => {
+  try {
+    const data = await getData.getAll('data_chapters')
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
 })
 
-router.get('/colors', (req, res) => {
-  res.send('<h1>TODOS LOS COLORs</h1>')
+router.get('/chapters/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await getData.getById('data_chapters', 'chapter_id', id)
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
 })
 
-router.get('/raritys', (req, res) => {
-  res.send('<h1>TODOS LOS RARITYs</h1>')
+router.get('/colors', async (req, res) => {
+  try {
+    const data = await getData.getAll('data_colors')
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
 })
 
-router.get('/types', (req, res) => {
-  res.send('<h1>TODOS LOS TYPESs</h1>')
+router.get('/colors/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await getData.getById('data_colors', 'color_id', id)
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
+})
+
+router.get('/raritys', async (req, res) => {
+  try {
+    const data = await getData.getAll('data_rarity')
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
+})
+
+router.get('/raritys/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await getData.getById('data_rarity', 'rarity_id', id)
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
+})
+
+router.get('/types', async (req, res) => {
+  try {
+    const data = await getData.getAll('data_type')
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
+})
+
+
+router.get('/types/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await getData.getById('data_type', 'type_id', id)
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Error al obtener datos de Supabase:', error);
+    res.status(500).json({ error: 'Error al obtener datos de Supabase' });
+  }
 })
 
 export default router
